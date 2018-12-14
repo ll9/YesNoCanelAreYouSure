ae2533063c396147e4df6c702075a954957b25f4
ll9 <llwierer@web.de>
2018-12-14 22:34:59 +0100

    Basic View

--- YesNoCanelAreYouSure/Form1.Designer.cs
+++ YesNoCanelAreYouSure/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TriggerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TriggerButton
+            // 
+            this.TriggerButton.Location = new System.Drawing.Point(104, 64);
+            this.TriggerButton.Name = "TriggerButton";
+            this.TriggerButton.Size = new System.Drawing.Size(75, 23);
+            this.TriggerButton.TabIndex = 0;
+            this.TriggerButton.Text = "Trigger";
+            this.TriggerButton.UseVisualStyleBackColor = true;
+            this.TriggerButton.Click += new System.EventHandler(this.TriggerButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TriggerButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button TriggerButton;
     }
 }
 
--- YesNoCanelAreYouSure/Form1.cs
+++ YesNoCanelAreYouSure/Form1.cs
@@ -16,5 +16,10 @@ namespace YesNoCanelAreYouSure
         {
             InitializeComponent();
         }
+
+        private void TriggerButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
